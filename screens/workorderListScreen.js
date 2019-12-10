dc894d6634152ffc90b78c80bdabf0b3d310fe10
Mikael Ahlström <mikaelahlstrom@Mikaels-MBP.wlan>
2019-12-10 21:26:27 +0200

    improve workorder list visuals

--- screens/workorderListScreen.js
+++ screens/workorderListScreen.js
@@ -1,32 +1,35 @@
-import React from 'react';
-import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
-import { FlatList, TouchableHighlight, TextInput } from 'react-native-gesture-handler';
-import Constants from 'expo-constants';
-import { MaterialCommunityIcons } from '@expo/vector-icons'
+import React from "react"
+import { StyleSheet, Text, View, ActivityIndicator } from "react-native"
+import {
+  FlatList,
+  TouchableHighlight,
+  TextInput
+} from "react-native-gesture-handler"
+import Constants from "expo-constants"
+import { MaterialCommunityIcons } from "@expo/vector-icons"
 
 export default class WorkorderListScreen extends React.Component {
-
   constructor() {
     super()
     this.state = {
       data: null,
       isLoaded: false,
-      visibleData: null,
+      visibleData: null
     }
   }
 
   onPress(id) {
-    this.props.navigation.navigate('Workorder', { id })
+    this.props.navigation.navigate("Workorder", { id })
   }
 
   componentDidMount() {
-    fetch('https://upickup.herokuapp.com/workorders')
+    fetch("https://upickup.herokuapp.com/workorders")
       .then(res => res.json())
       .then(res => {
         this.setState({
           data: res,
           isLoaded: true,
-          visibleData: res,
+          visibleData: res
         })
       })
   }
@@ -35,22 +38,29 @@ export default class WorkorderListScreen extends React.Component {
     if (!this.state.isLoaded) {
       return (
         <View style={styles.container}>
-          <ActivityIndicator size='large'/>
+          <ActivityIndicator size="large" />
         </View>
       )
     }
     return (
       <View style={styles.container}>
-        <TextInput onChangeText={text => {
-          this.setState({
-            visibleData: this.state.data.filter(workorder => {
-              return workorder.id.includes(text.toLowerCase())
+        <TextInput
+          onChangeText={text => {
+            this.setState({
+              visibleData: this.state.data.filter(workorder => {
+                return workorder.id.includes(text.toLowerCase())
+              })
             })
-          })
-        }} />
+          }}
+        />
         <FlatList
           data={this.state.visibleData}
-          renderItem={({ item }) => <WorkorderListItem workorder={item} onPress={this.onPress.bind(this, item.id)} />}
+          renderItem={({ item }) => (
+            <WorkorderListItem
+              workorder={item}
+              onPress={this.onPress.bind(this, item.id)}
+            />
+          )}
         />
       </View>
     )
@@ -58,57 +68,72 @@ export default class WorkorderListScreen extends React.Component {
 }
 
 class WorkorderListItem extends React.Component {
-
   render() {
     return (
-
       <View style={styles.listItem}>
-        <Text style={styles.listItemLabel}>Workorder</Text>
-        <Text style={styles.listText}>
-          {`${this.props.workorder.id}`}
-        </Text>
-        <TouchableHighlight underlayColor='E3E3E3' onPress={this.props.onPress}>
+        <View style={styles.workorderInfo}>
+          <Text style={styles.listItemLabel}>Workorder</Text>
+          <Text style={styles.listText}>{`${this.props.workorder.id}`}</Text>
+        </View>
+        <TouchableHighlight underlayColor="E3E3E3" onPress={this.props.onPress}>
           <View style={styles.iconContainer}>
-            <MaterialCommunityIcons style={styles.center} name='dropbox' size={35} />
-            <Text style={styles.center}>{`${this.props.workorder.items.length} items`}</Text>
+            <MaterialCommunityIcons
+              name="dropbox"
+              size={24}
+            />
+            <Text
+              style={styles.itemCount}
+            >{`${this.props.workorder.items.length} items`}</Text>
           </View>
         </TouchableHighlight>
       </View>
-
     )
   }
 }
 
-
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'stretch',
-    justifyContent: 'center',
-    marginTop: Constants.statusBarHeight,
+    alignItems: "stretch",
+    justifyContent: "center",
+    marginTop: Constants.statusBarHeight
   },
   listItem: {
     flex: 1,
-    borderBottomColor: '#F0F0F0',
-    borderTopColor: '#fff',
+    borderBottomColor: "#F0F0F0",
+    borderTopColor: "#fff",
     borderWidth: 1,
     flexDirection: "row",
-    paddingTop: 8,
-    paddingBottom: 8
+    justifyContent: "center",
+    padding: 16
   },
   listText: {
-    fontSize: 16,
-    marginRight:'auto'
+    fontSize: 18,
+    fontWeight: "bold",
+    marginTop: 4
   },
   iconContainer: {
-    marginRight: 20
+    flex: 1,
+    width: 96,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingRight: 16,
   },
-  center:{
-    alignSelf:"center"
+  itemCount: {
+    alignSelf: "center",
+    color: "#07051D65",
+    fontWeight: "700",
+    marginLeft: 8
   },
   listItemLabel: {
     fontSize: 12,
-    color: '#07051D65'
+    color: "#07051D65"
+  },
+  workorderInfo: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    paddingLeft: 8
   }
-});
+})
