069b8108787584cfccbead0e350a06efb00bdf7a
Mikael Ahlström <mikaelahlstrom@Mikaels-MBP.wlan>
2019-12-11 23:54:04 +0200

    extract styles and improve itemlist in workorder

--- screens/workorderListScreen.js
+++ screens/workorderListScreen.js
@@ -1,12 +1,12 @@
 import React from "react"
-import { StyleSheet, Text, View, ActivityIndicator } from "react-native"
+import { Text, View, ActivityIndicator } from "react-native"
 import {
   FlatList,
   TouchableHighlight,
   TextInput
 } from "react-native-gesture-handler"
-import Constants from "expo-constants"
 import { MaterialCommunityIcons } from "@expo/vector-icons"
+import { styles } from '../Styles.js'
 
 export default class WorkorderListScreen extends React.Component {
   constructor() {
@@ -72,7 +72,7 @@ class WorkorderListItem extends React.Component {
     return (
       <View style={styles.listItem}>
         <View style={styles.workorderInfo}>
-          <Text style={styles.listItemLabel}>Workorder</Text>
+          <Text style={styles.label}>Workorder</Text>
           <Text style={styles.listText}>{`${this.props.workorder.id}`}</Text>
         </View>
         <TouchableHighlight underlayColor="E3E3E3" onPress={this.props.onPress}>
@@ -82,7 +82,7 @@ class WorkorderListItem extends React.Component {
               size={24}
             />
             <Text
-              style={styles.itemCount}
+              style={styles.bodyBold}
             >{`${this.props.workorder.items.length} items`}</Text>
           </View>
         </TouchableHighlight>
@@ -90,50 +90,3 @@ class WorkorderListItem extends React.Component {
     )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "stretch",
-    justifyContent: "center",
-    marginTop: Constants.statusBarHeight
-  },
-  listItem: {
-    flex: 1,
-    borderBottomColor: "#F0F0F0",
-    borderTopColor: "#fff",
-    borderWidth: 1,
-    flexDirection: "row",
-    justifyContent: "center",
-    padding: 16
-  },
-  listText: {
-    fontSize: 18,
-    fontWeight: "bold",
-    marginTop: 4
-  },
-  iconContainer: {
-    flex: 1,
-    width: 96,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    paddingRight: 16,
-  },
-  itemCount: {
-    alignSelf: "center",
-    color: "#07051D65",
-    fontWeight: "700",
-    marginLeft: 8
-  },
-  listItemLabel: {
-    fontSize: 12,
-    color: "#07051D65"
-  },
-  workorderInfo: {
-    flex: 1,
-    flexDirection: "column",
-    justifyContent: "center",
-    paddingLeft: 8
-  }
-})
--- screens/workorderScreen.js
+++ screens/workorderScreen.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import { StyleSheet, Text, View, ActivityIndicator, Modal, Button, Picker } from 'react-native';
-import { FlatList, TouchableHighlight, TextInput } from 'react-native-gesture-handler';
-import * as WebBrowser from 'expo-web-browser';
-import Constants from 'expo-constants';
-import { MaterialIcons } from '@expo/vector-icons'
-
+import React from "react"
+import {
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+  Modal,
+  Button,
+  Picker
+} from "react-native"
+import {
+  FlatList,
+  TouchableHighlight,
+  TextInput
+} from "react-native-gesture-handler"
+import * as WebBrowser from "expo-web-browser"
+import Constants from "expo-constants"
+import { MaterialIcons } from "@expo/vector-icons"
+import { styles } from "../Styles.js"
 
 export default class WorkorderScreen extends React.Component {
-
   constructor() {
     super()
     this.state = {
@@ -21,16 +32,18 @@ export default class WorkorderScreen extends React.Component {
   }
 
   componentDidMount() {
-    const id = this.props.navigation.getParam('id', 'error');
+    const id = this.props.navigation.getParam("id", "error")
     fetch(`https://upickup.herokuapp.com/workorders/${id}`)
       .then(res => res.json())
       .then(res => {
-        const list = res[0];
-        list.items = list.items.sort((a, b) => a.item.location > b.item.location ? 1 : -1)
+        const list = res[0]
+        list.items = list.items.sort((a, b) =>
+          a.item.location > b.item.location ? 1 : -1
+        )
         this.setState({
           data: list,
           isLoaded: true,
-          id: id,
+          id: id
         })
       })
   }
@@ -41,23 +54,29 @@ export default class WorkorderScreen extends React.Component {
   }
 
   setModalVisible(visible, item) {
-    console.log('test')
-    this.setState({ modalVisible: visible, modalItem: item, picker: "Select issue" });
+    console.log("test")
+    this.setState({
+      modalVisible: visible,
+      modalItem: item,
+      picker: "Select issue"
+    })
   }
 
   render() {
     if (!this.state.isLoaded) {
       return (
         <View style={styles.container}>
-          <ActivityIndicator size='large' />
-        </View>)
+          <ActivityIndicator size="large" />
+        </View>
+      )
     }
     return (
-      < View style={styles.container} >
+      <View style={styles.container}>
         <Modal
           animationType="slide"
           transparent={false}
-          visible={this.state.modalVisible}>
+          visible={this.state.modalVisible}
+        >
           <View style={{ marginTop: 22 }}>
             <View>
               <Text>Submit issue for:</Text>
@@ -68,22 +87,29 @@ export default class WorkorderScreen extends React.Component {
                   this.setState({ picker: itemValue })
                 }}
               >
-                <Picker.Item label='Item not at location' value='Item not at location' />
-                <Picker.Item label='Not enough in stock' value='Not enough in stock' />
-                <Picker.Item label='Item is damaged' value='Item is damaged' />
+                <Picker.Item
+                  label="Item not at location"
+                  value="Item not at location"
+                />
+                <Picker.Item
+                  label="Not enough in stock"
+                  value="Not enough in stock"
+                />
+                <Picker.Item label="Item is damaged" value="Item is damaged" />
               </Picker>
-              <Button onPress={() => {
-                console.log('close')
-                this.setModalVisible(false, null);
-              }}
-                title='Cancel'
+              <Button
+                onPress={() => {
+                  console.log("close")
+                  this.setModalVisible(false, null)
+                }}
+                title="Cancel"
               />
               <Button
                 onPress={() => {
-                  console.log('close')
-                  this.setModalVisible(false, null);
+                  console.log("close")
+                  this.setModalVisible(false, null)
                 }}
-                title='Send'
+                title="Send"
               />
             </View>
           </View>
@@ -91,10 +117,15 @@ export default class WorkorderScreen extends React.Component {
 
         <FlatList
           data={this.state.data.items}
-          renderItem={({ item }) => <WorkorderItem item={item} onPress={this.onPressLoc.bind(this, item.item.url)}
-            onPress2={this.setModalVisible.bind(this)} />}
+          renderItem={({ item }) => (
+            <WorkorderItem
+              item={item}
+              onPress={this.onPressLoc.bind(this, item.item.url)}
+              onPress2={this.setModalVisible.bind(this)}
+            />
+          )}
         />
-      </View >
+      </View>
     )
   }
 }
@@ -108,82 +139,30 @@ class WorkorderItem extends React.Component {
   }
 
   render() {
-    const a = this.state.done ? styles.listItemDone :styles.listItem
+    const a = this.state.done ? styles.listItemDone : styles.listItem
     return (
       <View style={a}>
-          <Text style={styles.name} onPress={()=> this.setState({done: !this.state.done})}>
-            {this.props.item.item.name}
+        <View style={styles.test}>
+          <TouchableHighlight
+            underlayColor="gray"
+            onPress={() => this.props.onPress2(true, this.props.item.item.name)}
+          >
+            <View style={styles.icon}>
+              <MaterialIcons name="warning" size={24} color="#AAA" />
+            </View>
+          </TouchableHighlight>
+          <Text
+            style={styles.body}
+            onPress={() => this.setState({ done: !this.state.done })}
+          >
+            {this.props.item.qty} x {this.props.item.item.name}
           </Text>
-        <Text style={styles.qty}>
-          {this.props.item.qty}
-        </Text>
-        <View style={styles.icons}>
-          <TouchableHighlight underlayColor='gray' onPress={this.props.onPress}>
-            <View style={styles.icon}>
-              <MaterialIcons name='location-on' size={35} />
-              <Text>
-                {this.props.item.item.location}
-              </Text>
-            </View>
-          </TouchableHighlight >
-          <TouchableHighlight underlayColor='gray' onPress={() => this.props.onPress2(true, this.props.item.item.name)}>
-            <View style={styles.icon}>
-              <MaterialIcons name='warning' size={35} color='#d6d331' />
-              <Text>Report</Text>
-            </View>
-          </TouchableHighlight >
         </View>
-      </View >
+
+        <TouchableHighlight underlayColor="gray" onPress={this.props.onPress}>
+          <Text style={styles.listText}>{this.props.item.item.location}</Text>
+        </TouchableHighlight>
+      </View>
     )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'stretch',
-    justifyContent: 'center',
-    marginTop: Constants.statusBarHeight,
-  },
-  listItem: {
-    flexDirection: "row",
-    fontSize: 35,
-    borderColor: 'black',
-    borderWidth: 1,
-    alignItems: "center",
-    justifyContent: 'space-between',
-    flexWrap: 'nowrap',
-    paddingLeft: 5,
-    paddingRight: 5
-  },
-  listItemDone: {
-    flexDirection: "row",
-    fontSize: 35,
-    borderColor: 'black',
-    borderWidth: 1,
-    alignItems: "center",
-    justifyContent: 'space-between',
-    flexWrap: 'nowrap',
-    paddingLeft: 5,
-    paddingRight: 5,
-    backgroundColor:"#bdffdb"
-  },
-  name: {
-    fontSize: 35,
-    flex: 8
-  },
-  icons: {
-    flex: 2,
-    flexDirection: 'column',
-    justifyContent: "center",
-    alignItems: 'center',
-  },
-  icon: {
-    flexDirection: 'column',
-    justifyContent: "center",
-    alignItems: 'center',
-  },
-  qty: {
-    fontSize: 40
-  }
-});
